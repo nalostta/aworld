0a5784b4b50ca41dea74652d7f64e0825c797679
nalostta <shah.manthan38@gmail.com>
2025-04-19 14:40:22 -0700

    modified secret key line

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import uuid
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'default-secret-key')
 socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
 
 # Store active players
